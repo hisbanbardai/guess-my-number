c44b14a3ef92a6dc032537cad232bb9be3f3745e
hisbanshiraz <hisbanbardai@gmail.com>
2023-12-30 21:49:03 -0700

    Implement logic of setting highscore

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 'use strict';
 
 const number = Math.floor(Math.random() * 20 + 1);
-console.log(number);
+let score = 20;
+
 document.querySelector('.check').addEventListener('click', () => {
   const userInput = Number(document.querySelector('.guess').value);
 
@@ -9,9 +10,14 @@ document.querySelector('.check').addEventListener('click', () => {
     document.querySelector('.message').textContent = '🛑No number!!';
   } else if (userInput === number) {
     document.querySelector('.message').textContent = 'Correct🏆';
+    document.querySelector('.highscore').textContent = score;
   } else if (userInput > number) {
     document.querySelector('.message').textContent = 'Too high📈';
+    score -= 1;
+    document.querySelector('.score').textContent = score;
   } else {
     document.querySelector('.message').textContent = 'Too low📉';
+    score -= 1;
+    document.querySelector('.score').textContent = score;
   }
 });
