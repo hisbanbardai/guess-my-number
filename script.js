22306f439e0a2a9208e7c8b4633c0f2a99827bd0
hisbanshiraz <hisbanbardai@gmail.com>
2023-12-30 21:36:33 -0700

    Implement logic of guessing the number

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const number = Math.floor(Math.random() * 20 + 1);
+console.log(number);
+document.querySelector('.check').addEventListener('click', () => {
+  const userInput = Number(document.querySelector('.guess').value);
+
+  if (!userInput) {
+    document.querySelector('.message').textContent = '🛑No number!!';
+  } else if (userInput === number) {
+    document.querySelector('.message').textContent = 'Correct🏆';
+  } else if (userInput > number) {
+    document.querySelector('.message').textContent = 'Too high📈';
+  } else {
+    document.querySelector('.message').textContent = 'Too low📉';
+  }
+});
